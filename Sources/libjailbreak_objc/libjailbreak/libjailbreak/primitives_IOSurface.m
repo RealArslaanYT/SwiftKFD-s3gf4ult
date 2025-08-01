88b2ad295b12676dd2ed9e53bc08b320dab71ea2
Arslaan Pathan <xminecrafterfun@gmail.com>
2025-08-01 22:09:11 +1200

    more prefixes

--- Sources/libjailbreak_objc/libjailbreak/libjailbreak/primitives_IOSurface.m
+++ Sources/libjailbreak_objc/libjailbreak/libjailbreak/primitives_IOSurface.m
@@ -7,83 +7,83 @@
 #import <IOSurface/IOSurfaceRef.h>
 #import <CoreGraphics/CoreGraphics.h>
 
-uint64_t IOSurfaceRootUserClient_get_surfaceClientById(uint64_t rootUserClient, uint32_t surfaceId)
+uint64_t jb_IOSurfaceRootUserClient_get_surfaceClientById(uint64_t rootUserClient, uint32_t surfaceId)
 {
 	uint64_t surfaceClientsArray = jb_kread_ptr(rootUserClient + 0x118);
 	return jb_kread_ptr(surfaceClientsArray + (sizeof(uint64_t)*surfaceId));
 }
 
-uint64_t IOSurfaceClient_get_surface(uint64_t surfaceClient)
+uint64_t jb_IOSurfaceClient_get_surface(uint64_t surfaceClient)
 {
 	return jb_kread_ptr(surfaceClient + 0x40);
 }
 
-uint64_t IOSurfaceSendRight_get_surface(uint64_t surfaceSendRight)
+uint64_t jb_IOSurfaceSendRight_get_surface(uint64_t surfaceSendRight)
 {
 	return jb_kread_ptr(surfaceSendRight + 0x18);	
 }
 
-uint64_t IOSurface_get_ranges(uint64_t surface)
+uint64_t jb_IOSurface_get_ranges(uint64_t surface)
 {
 	return jb_kread_ptr(surface + 0x3e0);
 }
 
-void IOSurface_set_ranges(uint64_t surface, uint64_t ranges)
+void jb_IOSurface_set_ranges(uint64_t surface, uint64_t ranges)
 {
 	jb_kwrite64(surface + 0x3e0, ranges);
 }
 
-uint64_t IOSurface_get_memoryDescriptor(uint64_t surface)
+uint64_t jb_IOSurface_get_memoryDescriptor(uint64_t surface)
 {
 	return jb_kread_ptr(surface + 0x38);
 }
 
-uint64_t IOMemoryDescriptor_get_ranges(uint64_t memoryDescriptor)
+uint64_t jb_IOMemoryDescriptor_get_ranges(uint64_t memoryDescriptor)
 {
 	return jb_kread_ptr(memoryDescriptor + 0x60);
 }
 
-uint64_t IOMemorydescriptor_get_size(uint64_t memoryDescriptor)
+uint64_t jb_IOMemorydescriptor_get_size(uint64_t memoryDescriptor)
 {
 	return jb_kread64(memoryDescriptor + 0x50);
 }
 
-void IOMemoryDescriptor_set_size(uint64_t memoryDescriptor, uint64_t size)
+void jb_IOMemoryDescriptor_set_size(uint64_t memoryDescriptor, uint64_t size)
 {
 	jb_kwrite64(memoryDescriptor + 0x50, size);
 }
 
-void IOMemoryDescriptor_set_wired(uint64_t memoryDescriptor, bool wired)
+void jb_IOMemoryDescriptor_set_wired(uint64_t memoryDescriptor, bool wired)
 {
 	jb_kwrite8(memoryDescriptor + 0x88, wired);
 }
 
-uint32_t IOMemoryDescriptor_get_flags(uint64_t memoryDescriptor)
+uint32_t jb_IOMemoryDescriptor_get_flags(uint64_t memoryDescriptor)
 {
 	return jb_kread32(memoryDescriptor + 0x20);
 }
 
-void IOMemoryDescriptor_set_flags(uint64_t memoryDescriptor, uint32_t flags)
+void jb_IOMemoryDescriptor_set_flags(uint64_t memoryDescriptor, uint32_t flags)
 {
 	jb_kwrite8(memoryDescriptor + 0x20, flags);
 }
 
-void IOMemoryDescriptor_set_memRef(uint64_t memoryDescriptor, uint64_t memRef)
+void jb_IOMemoryDescriptor_set_memRef(uint64_t memoryDescriptor, uint64_t memRef)
 {
 	jb_kwrite64(memoryDescriptor + 0x28, memRef);
 }
 
-uint64_t IOSurface_get_rangeCount(uint64_t surface)
+uint64_t jb_IOSurface_get_rangeCount(uint64_t surface)
 {
 	return jb_kread_ptr(surface + 0x3e8);
 }
 
-void IOSurface_set_rangeCount(uint64_t surface, uint32_t rangeCount)
+void jb_IOSurface_set_rangeCount(uint64_t surface, uint32_t rangeCount)
 {
 	jb_kwrite32(surface + 0x3e8, rangeCount);
 }
 
-static mach_port_t IOSurface_map_getSurfacePort(uint64_t magic)
+static mach_port_t jb_IOSurface_map_getSurfacePort(uint64_t magic)
 {
 	IOSurfaceRef surfaceRef = IOSurfaceCreate((__bridge CFDictionaryRef)@{
 		(__bridge NSString *)kIOSurfaceWidth : @120,
@@ -97,37 +97,37 @@ static mach_port_t IOSurface_map_getSurfacePort(uint64_t magic)
 	return port;
 }
 
-int IOSurface_map(uint64_t pa, uint64_t size, void **uaddr)
+int jb_IOSurface_map(uint64_t pa, uint64_t size, void **uaddr)
 {
-	mach_port_t surfaceMachPort = IOSurface_map_getSurfacePort(1337);
+	mach_port_t surfaceMachPort = jb_IOSurface_map_getSurfacePort(1337);
 
 	uint64_t surfaceSendRight = task_get_ipc_port_kobject(task_self(), surfaceMachPort);
-	uint64_t surface = IOSurfaceSendRight_get_surface(surfaceSendRight);
-	uint64_t desc = IOSurface_get_memoryDescriptor(surface);
-	uint64_t ranges = IOMemoryDescriptor_get_ranges(desc);
+	uint64_t surface = jb_IOSurfaceSendRight_get_surface(surfaceSendRight);
+	uint64_t desc = jb_IOSurface_get_memoryDescriptor(surface);
+	uint64_t ranges = jb_IOMemoryDescriptor_get_ranges(desc);
 
 	jb_kwrite64(ranges, pa);
 	jb_kwrite64(ranges+8, size);
 
-	IOMemoryDescriptor_set_size(desc, size);
+    jb_IOMemoryDescriptor_set_size(desc, size);
 
 	jb_kwrite64(desc + 0x70, 0);
 	jb_kwrite64(desc + 0x18, 0);
 	jb_kwrite64(desc + 0x90, 0);
 
-	IOMemoryDescriptor_set_wired(desc, true);
+    jb_IOMemoryDescriptor_set_wired(desc, true);
 
-	uint32_t flags = IOMemoryDescriptor_get_flags(desc);
-	IOMemoryDescriptor_set_flags(desc, (flags & ~0x410) | 0x20);
+	uint32_t flags = jb_IOMemoryDescriptor_get_flags(desc);
+    jb_IOMemoryDescriptor_set_flags(desc, (flags & ~0x410) | 0x20);
 
-	IOMemoryDescriptor_set_memRef(desc, 0);
+    jb_IOMemoryDescriptor_set_memRef(desc, 0);
 
 	IOSurfaceRef mappedSurfaceRef = IOSurfaceLookupFromMachPort(surfaceMachPort);
 	*uaddr = IOSurfaceGetBaseAddress(mappedSurfaceRef);
 	return 0;
 }
 
-static mach_port_t IOSurface_kalloc_getSurfacePort(uint64_t size)
+static mach_port_t jb_IOSurface_kalloc_getSurfacePort(uint64_t size)
 {
 	uint64_t allocSize = 0x10;
 	uint64_t *addressRangesBuf = (uint64_t *)malloc(size);
@@ -146,15 +146,15 @@ static mach_port_t IOSurface_kalloc_getSurfacePort(uint64_t size)
 	return port;
 }
 
-uint64_t IOSurface_kalloc(uint64_t size, bool leak)
+uint64_t jb_IOSurface_kalloc(uint64_t size, bool leak)
 {
 	while (true) {
 		uint64_t allocSize = max(size, 0x10000);
-		mach_port_t surfaceMachPort = IOSurface_kalloc_getSurfacePort(allocSize);
+		mach_port_t surfaceMachPort = jb_IOSurface_kalloc_getSurfacePort(allocSize);
 
 		uint64_t surfaceSendRight = task_get_ipc_port_kobject(task_self(), surfaceMachPort);
-		uint64_t surface = IOSurfaceSendRight_get_surface(surfaceSendRight);
-		uint64_t va = IOSurface_get_ranges(surface);
+		uint64_t surface = jb_IOSurfaceSendRight_get_surface(surfaceSendRight);
+		uint64_t va = jb_IOSurface_get_ranges(surface);
 
 		if (kvtophys(va + allocSize) != 0) {
 			mach_port_deallocate(mach_task_self(), surfaceMachPort);
@@ -164,8 +164,8 @@ uint64_t IOSurface_kalloc(uint64_t size, bool leak)
 		if (va == 0) continue;
 
 		if (leak) {
-			IOSurface_set_ranges(surface, 0);
-			IOSurface_set_rangeCount(surface, 0);
+            jb_IOSurface_set_ranges(surface, 0);
+            jb_IOSurface_set_rangeCount(surface, 0);
 		}
 
 		return va + (allocSize - size);
@@ -174,9 +174,9 @@ uint64_t IOSurface_kalloc(uint64_t size, bool leak)
 	return 0;
 }
 
-int IOSurface_kalloc_global(uint64_t *addr, uint64_t size)
+int jb_IOSurface_kalloc_global(uint64_t *addr, uint64_t size)
 {
-	uint64_t alloc = IOSurface_kalloc(size, true);
+	uint64_t alloc = jb_IOSurface_kalloc(size, true);
 	if (alloc != 0) {
 		*addr = alloc;
 		return 0;
@@ -184,9 +184,9 @@ int IOSurface_kalloc_global(uint64_t *addr, uint64_t size)
 	return -1;
 }
 
-int IOSurface_kalloc_local(uint64_t *addr, uint64_t size)
+int jb_IOSurface_kalloc_local(uint64_t *addr, uint64_t size)
 {
-	uint64_t alloc = IOSurface_kalloc(size, false);
+	uint64_t alloc = jb_IOSurface_kalloc(size, false);
 	if (alloc != 0) {
 		*addr = alloc;
 		return 0;
@@ -194,7 +194,7 @@ int IOSurface_kalloc_local(uint64_t *addr, uint64_t size)
 	return -1;
 }
 
-void libjailbreak_IOSurface_primitives_init(void)
+void jb_libjailbreak_IOSurface_primitives_init(void)
 {
 	IOSurfaceRef surfaceRef = IOSurfaceCreate((__bridge CFDictionaryRef)@{
 		(__bridge NSString *)kIOSurfaceWidth : @120,
@@ -207,7 +207,7 @@ void libjailbreak_IOSurface_primitives_init(void)
 	}
 	CFRelease(surfaceRef);
 
-	gPrimitives.kalloc_global = IOSurface_kalloc_global;
-	gPrimitives.kalloc_local  = IOSurface_kalloc_local;
-	gPrimitives.kmap          = IOSurface_map;
+	gPrimitives.kalloc_global = jb_IOSurface_kalloc_global;
+	gPrimitives.kalloc_local  = jb_IOSurface_kalloc_local;
+	gPrimitives.kmap          = jb_IOSurface_map;
 }
